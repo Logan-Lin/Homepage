1d7b10b2fa9569277ebfcd5197fe88bc17acd254
Yan Lin <lyan@cs.aau.dk>
2025-03-14 10:06:07 +0100

    Add dedicated publication page

--- index.js
+++ index.js
@@ -1,79 +1,20 @@
-function renderPublication(pub, type) {
-  return `<div class="list-group-item px-0">
-              <div class="d-flex justify-content-between align-items-center mb-1">
-                <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
-                  ${pub.venue} <span class="text-muted">|</span> ${pub.year}
-                </p>
-                <div class="d-flex gap-2">
-                  ${renderLinks(pub.links)}
-                </div>
-              </div>
-              <h5 class="mb-1 paper-title">${pub.title}</h5>
-              <p class="card-text mb-auto author-name">${pub.authors}</p>
-            </div>`;
-}
-
-function renderProject(project, type) {
-  return `<div class="list-group-item px-0">
-            <div class="d-flex justify-content-between align-items-center mb-1">
-              <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
-                ${project.venue}
-              </p>
-              <div class="d-flex gap-2">
-                ${renderLinks(project.links)}
-              </div>
-            </div>
-            <h5 class="mb-1 paper-title">${project.title}</h5>
-            <p class="card-text mb-auto project-desc">${project.desc}</p>
-          </div>`;
-}
-
-function renderPresentation(present, type) {
-  return `<div class="list-group-item px-0">
-            <div class="d-flex justify-content-between align-items-center mb-1">
-              <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
-                ${present.venue} <span class="text-muted">|</span> ${present.location}
-              </p>
-              <div class="d-flex gap-2">
-                ${renderLinks(present.links)}
-              </div>
-            </div>
-            <h5 class="mb-1 paper-title">${present.title}</h5>
-          </div>`;
-}
-
-function renderLinks(links) {
-  let linksHtml = '';
-  for (const [key, url] of Object.entries(links)) {
-    // Convert camelCase to Title Case (e.g., paperLink -> Paper)
-    linksHtml += `<a class="icon-link icon-link-hover paper-link link-secondary" href="${url}" target="_blank" rel="noopener noreferrer">${key}</a>`;
-  }
-  return linksHtml;
-}
-
-function renderList(list) {
-  return `<ul class="mb-2 px-2 px-md-4">
-            ${list.map(item => `<li class="">${item}</li>`).join("")}
-          </ul>`;
-}
-
 $(document).ready(function() {
   const $primaryPublicationsContainer = $("#primary-publications");
   const $secondaryPublicationsContainer = $("#secondary-publications");
   if ($primaryPublicationsContainer.length) {
-    $primaryPublicationsContainer.html(profileData.primaryPublications.map(pub => renderPublication(pub, 'primary')).join(""));
+    $primaryPublicationsContainer.html(profileData.primaryPublications.slice(0, 10).map(pub => renderPublication(pub, 'primary')).join(""));
   }
   if ($secondaryPublicationsContainer.length) {
-    $secondaryPublicationsContainer.html(profileData.secondaryPublications.map(renderPublication).join(""));
+    $secondaryPublicationsContainer.html(profileData.secondaryPublications.slice(0, 10).map(pub => renderPublication(pub, 'secondary')).join(""));
   }
 
   const $primaryProjectsContainer = $("#primary-projects");
   const $secondaryProjectsContainer = $("#secondary-projects");
   if ($primaryProjectsContainer.length) {
-    $primaryProjectsContainer.html(profileData.primaryProjects.map(project => renderProject(project, 'primary')).join(""));
+    $primaryProjectsContainer.html(profileData.primaryProjects.slice(0, 10).map(project => renderProject(project, 'primary')).join(""));
   }
   if ($secondaryProjectsContainer.length) {
-    $secondaryProjectsContainer.html(profileData.secondaryProjects.map(renderProject).join(""));
+    $secondaryProjectsContainer.html(profileData.secondaryProjects.slice(0, 10).map(project => renderProject(project, 'secondary')).join(""));
   }
 
   const $presentationsContainer = $("#presentation-list");
@@ -99,6 +40,7 @@ $(document).ready(function() {
   $('#back-to-top').click(function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, 0);
+    window.location.href = '#';
     return false;
   });
 });
---
+++ render.js
@@ -0,0 +1,58 @@
+function renderPublication(pub, type) {
+  return `<div class="list-group-item px-0">
+              <div class="d-flex justify-content-between align-items-center mb-1">
+                <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
+                  ${pub.venue} <span class="text-muted">|</span> ${pub.year}
+                </p>
+                <div class="d-flex gap-2">
+                  ${renderLinks(pub.links)}
+                </div>
+              </div>
+              <h5 class="mb-1 paper-title">${pub.title}</h5>
+              <p class="card-text mb-auto author-name">${pub.authors}</p>
+            </div>`;
+}
+
+function renderProject(project, type) {
+  return `<div class="list-group-item px-0">
+            <div class="d-flex justify-content-between align-items-center mb-1">
+              <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
+                ${project.venue}
+              </p>
+              <div class="d-flex gap-2">
+                ${renderLinks(project.links)}
+              </div>
+            </div>
+            <h5 class="mb-1 paper-title">${project.title}</h5>
+            <p class="card-text mb-auto project-desc">${project.desc}</p>
+          </div>`;
+}
+
+function renderPresentation(present, type) {
+  return `<div class="list-group-item px-0">
+            <div class="d-flex justify-content-between align-items-center mb-1">
+              <p class="d-inline-block mb-0 venue-name ${type === 'primary' ? 'text-danger-emphasis' : 'text-primary-emphasis'}">
+                ${present.venue} <span class="text-muted">|</span> ${present.location}
+              </p>
+              <div class="d-flex gap-2">
+                ${renderLinks(present.links)}
+              </div>
+            </div>
+            <h5 class="mb-1 paper-title">${present.title}</h5>
+          </div>`;
+}
+
+function renderLinks(links) {
+  let linksHtml = '';
+  for (const [key, url] of Object.entries(links)) {
+    // Convert camelCase to Title Case (e.g., paperLink -> Paper)
+    linksHtml += `<a class="link icon-link icon-link-hover paper-link link-secondary" href="${url}" target="_blank" rel="noopener noreferrer">${key}</a>`;
+  }
+  return linksHtml;
+}
+
+function renderList(list) {
+  return `<ul class="mb-2 px-2 px-md-4">
+            ${list.map(item => `<li class="">${item}</li>`).join("")}
+          </ul>`;
+}
